07be1c87ba81f5bb975267d015f3a37cfa397cfe
uestcmy <369064521@qq.com>
2014-08-16 17:29:06 +0800

    Add select function to  the socket im mygl2

---
+++ NetworkLink_20140816.pro
@@ -0,0 +1,15 @@
+QT += opengl
+HEADERS += \
+    mygl2.h \
+    networklink.h \
+    ui_mygl2.h \
+    ui_networklink.h
+
+SOURCES += \
+    main.cpp \
+    mygl2.cpp \
+    networklink.cpp
+
+FORMS += \
+    mygl2.ui \
+    networklink.ui
--- mygl2.cpp
+++ mygl2.cpp
@@ -26,6 +26,9 @@
 #define LENGTH_OF_CORE 84
 #define SOCKET_REFRESH
 
+int ifrecvd = 0;
+
+char lastbuff[9000];
 char buff[LEN];
 char buff2[LEN];
 char buff3[LEN];
@@ -134,8 +137,6 @@ mygl2::mygl2(QWidget *parent) :
     recvfrom(sockser2,&str,9000,0,(struct sokaddr *)&addrrcv2,(socklen_t*)&intsize);
     qDebug() << str << endl;
 */
-
-
     translate = -7.0;
     xRot = yRot = zRot = 0.0;
     xRot += 20;
@@ -1581,7 +1582,15 @@ void mygl2::keyPressEvent(QKeyEvent *event)
     QGLWidget::keyPressEvent(event);
 }
 
+void reducemap(){
+    for( int i = 0 ; i < 4 ; i++ ){
+        for( int j = 0 ; j < 4 ; j++){
+            map[i][j] *= 0.5;
+        }
+    }
 
+
+}
 void mygl2::timerEvent(QTimerEvent *event){
     //socket run
      //int num_comma = 0;//counter of comma
@@ -1590,15 +1599,33 @@ void mygl2::timerEvent(QTimerEvent *event){
 
       memset(out_mygl2,0,LEN*5);
       int intsize=sizeof(sockaddr_in);
-      char buff[9000];
-      recvfrom(sockser,&buff,9000,0,(struct sockaddr *)&addrrcv,(socklen_t*)&intsize);
+
+
+
+
+
+
+      memcpy(lastbuff,buff,LEN);
+      fd_set fdR;
+      struct timeval timeout = {1,0};
+      FD_ZERO(&fdR);
+      FD_SET(sockser,&fdR);
+      switch( select(sockser+1,&fdR,NULL,NULL,&timeout) ){
+          case -1: break;
+          case 0: ifrecvd = -1;memcpy(buff,lastbuff,LEN);break;
+          default:ifrecvd = 1;
+                 recvfrom(sockser,&buff,9000,0,(struct sockaddr *)&addrrcv,(socklen_t*)&intsize);break;
+      }
+      if(ifrecvd == 1){
+      //recvfrom(sockser,&buff,9000,0,(struct sockaddr *)&addrrcv,(socklen_t*)&intsize);
+      //qDebug() << buff;
       qDebug() << "Counter is " << cnt++ << endl;
 
       search_core_data_mygl2_a(buff);// to out_mygl2_net_A
       if(endmark_a == 1){
          endmark_a = 0;
          //swap the order
-          for( int i = 0 ; i < 84 ; i ++){
+          for( int i = 0 ; i < 84 ; i ++){ //84*6=504
              int position = i * 6;
              char tmp;
              //swap
@@ -1612,15 +1639,14 @@ void mygl2::timerEvent(QTimerEvent *event){
              out_mygl2_a[position+3] = tmp;
           }//for
 
-         qDebug() << out_mygl2_a << endl;
+          qDebug() << out_mygl2_a << endl;
       }
-      for(int i = 1 ; i <= 20; i++ ){
+      for(int i = 1 ; i <= 20; i++ ){  // 20*4=80
           char mark[2];
           mark[0] = out_mygl2_a[i*12];
           mark[1] = out_mygl2_a[i*12+1];
           if( mark[0] == '1' && mark[1] == '4'){
              map[2][3] = charToint(&out_mygl2_a[i*12+3],9)*4;
-
           }else if( mark[0] == '2' && mark[1] == '4' ){
              map[3][2] = charToint(&out_mygl2_a[i*12+3],9)*4;
           }else if( mark[0] == '3' && mark[1] == '4' ){
@@ -1630,7 +1656,6 @@ void mygl2::timerEvent(QTimerEvent *event){
           }
       }//for i.
       memset(out_mygl2_a,0,9000);
-
 
       search_core_data_mygl2_b(buff);// to out_mygl2_net_B
       if(endmark_b == 1){
@@ -1779,6 +1804,12 @@ void mygl2::timerEvent(QTimerEvent *event){
       qDebug() << "map[3][1] is :" << map[3][1];
       qDebug() << "map[3][2] is :" << map[3][2];
       qDebug() << "map[3][3] is :" << map[3][3] <<endl;
+
+      }else if( ifrecvd == -1 ){
+          reducemap();
+
+      }
+
 /*
      while(1){
          char str[1000];
@@ -1791,7 +1822,7 @@ void mygl2::timerEvent(QTimerEvent *event){
                  //if(num_frame == 24){file.close();}
              }//if
              search_core_data_mygl2(buff);
-         }else if(continueflag_mygl2 == 1){
+         }else if(continueflag_mygl2reducemap == 1){
              search_core_data2_mygl2(buff);
          }//elif
          //qDebug() << buff << endl;
@@ -1824,7 +1855,7 @@ void mygl2::timerEvent(QTimerEvent *event){
      memset(out_mygl2_2,0,LEN*5);
 
      while(1){
-         char str[1000];
+         char str[1000];reducemap
          recvfrom(sockser,&buff,9000,0,(struct sockaddr *)&addrrcv,(socklen_t*)&intsize);
         // recvfrom(sockser2,&buff2,9000,0,(struct sockaddr *)&addrrcv2,(socklen_t*)&intsize);
          if( continueflag_mygl2_2 == 0 ){
@@ -1939,7 +1970,7 @@ void mygl2::timerEvent(QTimerEvent *event){
                //swap
                position ++;
                tmp = out_mygl2_4[position];
-               out_mygl2_4[position] = out_mygl2_4[position+3];
+               out_mygl2_4[position] = out_mygl2_4[position+3];out_mygl2_a
                out_mygl2_4[position+3] = tmp;
             }//for
 
@@ -1950,7 +1981,7 @@ void mygl2::timerEvent(QTimerEvent *event){
             break;
          }
      }//while
-
+reducemap
 
 
      memset(visit,0,sizeof(int)*16);
@@ -1966,7 +1997,7 @@ void mygl2::timerEvent(QTimerEvent *event){
             visit[3][2] ++;
          }else if( mark[0] == '3' && mark[1] == '4' ){
             map[3][3] = charToint(&out_mygl2[i*12+3],9);
-            visit[3][3] ++;
+            visit[3][3] ++;out_mygl2_a
          }else if( mark[0] == '0' && mark[1] == '4' ){
             map[2][2] = charToint(&out_mygl2[i*12+3],9);
             visit[2][2] ++;
@@ -1977,7 +2008,7 @@ void mygl2::timerEvent(QTimerEvent *event){
      for(int i = 1 ; i <= 20; i++ ){
          char mark[2];
          mark[0] = out_mygl2_2[i*12];
-         mark[1] = out_mygl2_2[i*12+1];
+         mark[1] = out_mygl2_2[i*12+reducemap1];
          if( mark[0] == '4' && mark[1] == '4'){
             map[2][0] = charToint(&out_mygl2_2[i*12+3],9);
             visit[2][0] ++;
@@ -2040,7 +2071,7 @@ void mygl2::timerEvent(QTimerEvent *event){
              if(visit[i][j] != 1){
                  flag = 1;
                  break;
-             }
+             }reducemap
          }
      }
      for(int i = 0 ; i < 4 ; i++ ){
@@ -2088,14 +2119,14 @@ void mygl2::timerEvent(QTimerEvent *event){
     glEnable (GL_LINE_SMOOTH );
     glHint (GL_POLYGON_SMOOTH , GL_NICEST);
 
+        histoPlot11(map[3][0]);histoPlot12(map[3][1]);histoPlot13(map[2][0]);histoPlot14(map[2][1]);
+        histoPlot21(map[3][2]);histoPlot22 (map[3][3]);histoPlot23(map[2][2]);histoPlot24(map[2][3]);
+        histoPlot31(map[1][0]);histoPlot32(map[1][1]);histoPlot33(map[0][0]);histoPlot34( map[0][1]);
+        histoPlot41(map[1][2]);histoPlot42(map[1][3]);histoPlot43(map[0][2]);histoPlot44(map[0][3]);
+        wallplot();
+        glEnd();
+        updateGL();
 
-    histoPlot11(map[3][0]);histoPlot12(map[3][1]);histoPlot13(map[2][0]);histoPlot14(map[2][1]);
-    histoPlot21(map[3][2]);histoPlot22 (map[3][3]);histoPlot23(map[2][2]);histoPlot24(map[2][3]);
-    histoPlot31(map[1][0]);histoPlot32(map[1][1]);histoPlot33(map[0][0]);histoPlot34( map[0][1]);
-    histoPlot41(map[1][2]);histoPlot42(map[1][3]);histoPlot43(map[0][2]);histoPlot44(map[0][3]);
-    wallplot();
-    glEnd();
-    updateGL();
     //floor
 
 
@@ -2448,7 +2479,7 @@ void search_core_data2_mygl2_4(  char* buff ){ // continue to send the data
 
 void search_core_data_mygl2_a(  char* buff ){ // FPGA_A
     int i;
-    int find = 0;//consider that there is no header?
+    int find = 0;//consider that there is no header???
     for( i = 0 ; i < LEN - 500 ; i++ ){
         //search the header of the core-frame
         if(buff[i] == 'a' && buff[i+1] == 'a' && buff[i+2] == ',' && buff[i+3] == 'a' && buff[i+4] == '0' && buff[i+5] == ',' && buff[i+6] == '2' && buff[i+7] == '8' && buff[i+8] == ',' && buff[i+9] == '4' && buff[i+10] == '0'){
@@ -2463,17 +2494,17 @@ void search_core_data_mygl2_a(  char* buff ){ // FPGA_A
     int cnt = 0;
     int cnt_comma = 0;
     //outposition = 0;
-    for( int j = i ;  ; j++){
-        if( buff[j] != '-' && buff[j] != '\n'){
+    for( int j = i ;  ; j++){// i is the start of data without header-a0aa2840
+        if( buff[j] != '-' && buff[j] != '\n'){  // avoid - and \n
             bottle[cnt++]  = buff[j];
             //out[outposition++] = buff[j];
         }//if
 
-        if( buff[j] == ',' ){
+        if( buff[j] == ',' ){// counter
             cnt_comma ++;
         }
 
-        if( cnt_comma == LENGTH_OF_CORE  ){
+        if( cnt_comma == LENGTH_OF_CORE  ){ // reach the end of one frame
            rest_num_comma_mygl2_a = 0;
            continueflag_mygl2_a = 0;
            endmark_a = 1;//find a total frame
@@ -2487,7 +2518,7 @@ void search_core_data_mygl2_a(  char* buff ){ // FPGA_A
         }
     }//for j
     //qDebug() <<"FIRST :" <<bottle <<endl;
-    strcat(out_mygl2_a,bottle);
+    strcat(out_mygl2_a,bottle); // out_mygl2_a <------ bottle
 }
 
 
@@ -2584,7 +2615,7 @@ void search_core_data_mygl2_d(  char* buff ){ // FPGA_A
     int find = 0;//consider that there is no header?
     for( i = 0 ; i < LEN - 500 ; i++ ){
         //search the header of the core-frame
-        if(buff[i] == 'a' && buff[i+1] == 'a' && buff[i+2] == ',' && buff[i+3] == 'a' && buff[i+4] == '0' && buff[i+5] == ',' && buff[i+6] == '2' && buff[i+7] == '8' && buff[i+8] == ',' && buff[i+9] == '9' && buff[i+10] == '0'){
+        if(buff[i] == 'a' && buff[i+1] == 'a' && buff[i+2] == ',' && buff[i+3] == 'a' && buff[i+4] == '0' && buff[i+5] == ',' && buff[i+6] == '2' && buff[i+7] == '8' && buff[i+8] == ',' && buff[i+9] == 'a' && buff[i+10] == '0'){
             find = 1;
             break;
         }
@@ -2613,7 +2644,7 @@ void search_core_data_mygl2_d(  char* buff ){ // FPGA_A
 
            break;
         }
-        else if( j == LEN){
+        else if( j == LEN){  // LEN is 9000
             rest_num_comma_mygl2_d = LENGTH_OF_CORE - cnt_comma;
             continueflag_mygl2_d = 1; //there is still some frames in the later buffer
             break;
